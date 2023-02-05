0caeeb0a6597dad431ca6c64267bc869d1c5b3c9
Andrey "Zed" Zaikin <zed.0xff@gmail.com>
2023-02-05 14:59:58 +0200

    switch to tabulo

--- Gemfile
+++ Gemfile
@@ -1,4 +1,4 @@
 source "https://rubygems.org"
 
-gem 'aws-sdk-ec2'
-gem 'terminal-table'
+gem 'aws-sdk-core'
+gem 'tabulo'
---
+++ lib/lsaws/utils.rb
@@ -0,0 +1,19 @@
+class Lsaws
+  module Utils
+		# copypasted from lib/active_support/core_ext/hash/keys.rb
+    def self._deep_transform_keys_in_object!(object, &block)
+      case object
+      when Hash
+        object.keys.each do |key|
+          value = object.delete(key)
+          object[yield(key)] = _deep_transform_keys_in_object!(value, &block)
+        end
+        object
+      when Array
+        object.map! { |e| _deep_transform_keys_in_object!(e, &block) }
+      else
+        object
+      end
+    end
+  end
+end
